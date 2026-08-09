8068a9527e723dcdee10258ec20a872e4eb1465a
Awe Morris <awe@noctvm.io>
2026-08-10 01:12:43 +0900

    hw/net: add the LGY-98 network interface
    Add the Melco LGY-98, the NE2000-compatible Ethernet board commonly used
    on PC-98.
    
    Signed-off-by: Awe Morris <awe@noctvm.io>

---
+++ include/hw/net/pc98-lgy98.h
@@ -0,0 +1,13 @@
+/*
+ * QEMU NEC PC-98 LGY-98 C-bus Ethernet
+ *
+ * Copyright (c) 2026 Awe Morris
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+#ifndef HW_NET_PC98_LGY98_H
+#define HW_NET_PC98_LGY98_H
+
+#define TYPE_PC98_LGY98 "pc98-lgy98"
+
+#endif
